ea8fbfb35108e7a2ce6ab0a1617da1c201017c77
Avi Goldman <avrahamymgoldman@gmail.com>
2016-05-27 12:30:14 -0400

    refactor to function

--- sparkpost-api.js
+++ sparkpost-api.js
@@ -1,11 +1,12 @@
-var sparky = {
-	_options: {
+var sparky = function() {
+	this._options: {
 		apiKey: null,
 		origin: 'https://api.sparkpost.com:443',
 		apiVersion: 'v1',
-		headers: {}
-	}
+		headers: {
+			"Content-Type: application/json"
+		}
+	};
 };
-
 
 export var Sparkpost = sparky;
